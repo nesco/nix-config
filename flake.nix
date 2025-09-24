f4df8da59da48321e9827dbefad53acc59ea18a5
Emmanuel FEDERBUSCH <emmanuel@federbusch.fr>
2025-09-24 15:33:06 +0200

    moving to the right nix-darwin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url  = "github:LnL7/nix-darwin";
+    nix-darwin.url  = "github:nix-darwin/nix-darwin";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
